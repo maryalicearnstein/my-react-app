ab5c773d16ec4c451e9514a136fb55b8bbbf4ea4
Mary Alice Arnstein <maryalicearnstein@Mary-Alices-MacBook-Pro-2.local>
2018-10-24 14:42:21 -0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Mary Alice Learns React',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>The SDL can use React</h1>
+    <p>We are whales!</p>
+    <p>Let's get this bread</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
